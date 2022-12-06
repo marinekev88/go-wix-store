b2823fd5bb837988825326887b7c94be70093101
Kevin M <marinekev88@gmail.com>
2022-12-05 21:12:07 -0700

    wip

--- product.go
+++ product.go
@@ -1,11 +1,15 @@
 package go_wix_store
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	. "github.com/marinekev88/go-wix-store/types"
+)
 
 type ProductService interface {
-	List(key string, query string) ([]string, error)
-	ListAll() ([]string, error)
-	Get(id string) (string, error)
+	List(key string, query string) (products Products, err error)
+	ListAll() (products Products, err error)
+	Get(id string) (Product, error)
 }
 
 type ProductServiceOp struct {
@@ -14,25 +18,36 @@ type ProductServiceOp struct {
 
 var _ ProductService = &ProductServiceOp{}
 
-func (p ProductServiceOp) List(key string, query string) ([]string, error) {
+func (p ProductServiceOp) List(key string, query string) (products Products, err error) {
 	res, _ := p.client.handler.R().
 		SetBody(fmt.Sprintf(`{"query":{"filter":"{\"%s\": \"%s\"}"}}`, key, query)).
 		Post("/query")
-	fmt.Println(res.Body())
 
-	return []string{"Hello", "Kitty"}, nil
+	if err = json.Unmarshal(res.Body(), &products); err != nil {
+		return
+	}
+
+	return
 }
 
-func (p ProductServiceOp) ListAll() ([]string, error) {
+func (p ProductServiceOp) ListAll() (products Products, err error) {
 	res, _ := p.client.handler.R().
 		Post("/query")
-	fmt.Println(res.Body())
-	return []string{"Hello", "Kitty"}, nil
+
+	if err = json.Unmarshal(res.Body(), &products); err != nil {
+		return
+	}
+
+	return
 }
 
-func (p ProductServiceOp) Get(id string) (string, error) {
+func (p ProductServiceOp) Get(id string) (product Product, err error) {
 	res, _ := p.client.handler.R().
 		Get(fmt.Sprintf("/%s", id))
-	fmt.Println(res.Body())
-	return "hello", nil
+
+	if err = json.Unmarshal(res.Body(), &product); err != nil {
+		return
+	}
+
+	return
 }
---
+++ types/product.go
@@ -0,0 +1,54 @@
+package types
+
+type Products struct {
+	ProductList []Product `json:"products,omitempty"`
+}
+
+type Product struct {
+	Name           string           `json:"name,omitempty"`
+	Visible        bool             `json:"visible,omitempty"`
+	Description    string           `json:"description,omitempty"`
+	Sku            string           `json:"sku,omitempty"`
+	Stock          Stock            `json:"stock,omitempty"`
+	Price          Price            `json:"price,omitempty"`
+	AdditionalInfo []AdditionalInfo `json:"additionalInfoSections,omitempty"`
+	ProductOptions []ProductOption  `json:"productOptions,omitempty"`
+	Discount       Discount         `json:"discount,omitempty"`
+}
+
+type Stock struct {
+	InventoryStatus string `json:"inventoryStatus,omitempty"`
+	InStock         bool   `json:"inStock,omitempty"`
+}
+
+type Price struct {
+	Currency        string  `json:"currency,omitempty"`
+	Price           float64 `json:"price,omitempty"`
+	DiscountedPrice float64 `json:"discountedPrice,omitempty"`
+	Formatted       struct {
+		Price           string `json:"price,omitempty"`
+		DiscountedPrice string `json:"discountedPrice,omitempty"`
+	} `json:"formatted,omitempty"`
+}
+
+type AdditionalInfo struct {
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+type ProductOption struct {
+	OptionType string          `json:"optionType,omitempty"`
+	Name       string          `json:"name,omitempty"`
+	Choices    []ProductChoice `json:"choices,omitempty"`
+}
+
+type ProductChoice struct {
+	Value       string `json:"value,omitempty"`
+	Description string `json:"description,omitempty"`
+	InStock     bool   `json:"inStock,omitempty"`
+}
+
+type Discount struct {
+	Type  string  `json:"type,omitempty"`
+	Value float64 `json:"value,omitempty"`
+}
